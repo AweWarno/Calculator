788cc64532b766756bbb8603e7b00002f9c47b3b
Alekseu F <awesome@warno.ru>
2024-02-14 22:55:13 +0300

    end2

--- Calculator.java
+++ Calculator.java
@@ -6,11 +6,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> {
-        if (y == 0) return 0;
-
-        return x / y;
-    };
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? 0 : x / y;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
